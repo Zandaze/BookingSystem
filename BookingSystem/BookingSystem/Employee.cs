0e5a8bd3a57c9406c9563561be78793d251b8303
Valdet <valdet_neziri@outlook.com>
2017-02-15 12:51:12 +0100

    All Classes Added

---
+++ BookingSystem/BookingSystem/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem
+{
+    class Employee
+    {
+        private string empId { get; set; }
+        private string name { get; set; }
+        private string adress { get; set; }
+        private string phoneNr { get; set; }
+
+    }
+}
